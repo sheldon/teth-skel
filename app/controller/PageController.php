3e84aaf39a659998820fb528e7e943676bd2e38c
Sheldon <sheldon.els@gmail.com>
2009-12-19 10:42:24 +0000

    change to new initial_view approach added docs to pagecontroller to explain

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -1,5 +1,20 @@
 <?php
+/**
+ * default main controller
+ * define your actions here, any defined methods will be accessible as public facing pages
+ */
 class PageController extends CoreController{
+
+  /**
+   * initial view to be rendered before others
+   * this view will be triggered by CoreApplication
+   * $this->original_path will be set to the original url the application was triggered with
+   * normal behaviour is to call a render on that var inside the initial_view
+   * allows you do run a general action before all other controller code
+   */
+  public $intial_view="application";
+  public function application(){}
+  
   public function index(){}
 }
 ?>
